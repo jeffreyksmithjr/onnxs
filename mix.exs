41e027907ff04f289ee648e891d6eb6b59997ee6
Jeff Smith <jeffreyksmithjr@gmail.com>
2018-04-07 17:09:57 -0400

    adding project metadata

--- mix.exs
+++ mix.exs
@@ -7,7 +7,18 @@ defmodule ONNXS.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      description: "ONNX interop for Elixir",
+      package: package,
       deps: deps()
+    ]
+  end
+  
+  def package do
+    [ name: :onnxs,
+      files: ["lib", "mix.exs"],
+      maintainers: ["Jeff Smith"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/jeffreyksmithjr/onnxs"},
     ]
   end
 
